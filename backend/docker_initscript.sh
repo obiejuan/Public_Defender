a96c3e782325edb18f0f77eaced475c25b72c400
Mackenzie Glynn <mglynn@ucsc.edu>
2017-04-17 12:35:19 -0700

    [CMPS-26] Wrapped docker some more so that it can be installed with a script and it handles nodejs

--- backend/docker_initscript.sh
+++ backend/docker_initscript.sh
@@ -3,3 +3,5 @@ apt-get install -y curl
 
 curl -sL https://deb.nodesource.com/setup_7.x | bash -
 apt-get install -y nodejs postgresql-9.5
+npm install
+node index.js
---
+++ backend/install_docker.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install docker-engine
