2445ac7dd41da792381316d4df602d23acb894d5
sslarkin <sslarkin@ucsc.edu>
2017-04-25 22:10:40 -0700

    Home button on settings page

--- frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/Settings.java
+++ frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/Settings.java
@@ -1,7 +1,9 @@
 package com.cmps115.public_defender;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+    public void goHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        setContentView(R.layout.activity_main);
+        startActivity(intent);
+    }
+
 }
