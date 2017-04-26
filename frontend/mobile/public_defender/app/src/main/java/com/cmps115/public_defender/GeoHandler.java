eaca059cb41f2c0c6bb1791128f67ba6348c5356
Oliver Davies <odavies@ucsc.edu>
2017-04-26 00:53:20 -0700

    Attempting to solve issues with the emulator and the location premissions. Still having issues. The emulator is pretty salty about geolocation.

--- frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/GeoHandler.java
+++ frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/GeoHandler.java
@@ -1,46 +1,45 @@
 package com.cmps115.public_defender;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.os.Bundle;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by Oliver Davies on 4/23/2017.
  */
 
-public class GeoHandler implements LocationListener
-{
+public class GeoHandler {
+
+    // Geolocation components
+    LocationManager geoManager;
+    AppCompatActivity activity;
+
     private double[] geoPosition = new double[2];
-    private boolean isProviderDefined = false;
 
-    public boolean hasGeolocation()
-    {
-        return isProviderDefined && ((geoPosition == null));
+    public GeoHandler(AppCompatActivity act, LocationManager lm) {
+        activity = act;
+        geoManager = lm;
     }
 
-    public double[] getGeolocation()
-    {
+    public boolean hasGeolocation() {
+        return true;
+    }
+
+    public double[] getGeolocation() {
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return geoPosition;
+        }
+
+        Location l = geoManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        Toast.makeText(activity, "Tried to find Geolocation", Toast.LENGTH_SHORT).show();
+        try {
+            geoPosition[0] = l.getLatitude();
+            geoPosition[1] = l.getLongitude();
+        } catch (NullPointerException e){}
         return geoPosition;
-    }
-
-    @Override
-    public void onLocationChanged(Location loc)
-    {
-        geoPosition[0] = loc.getLatitude();
-        geoPosition[1] = loc.getLongitude();
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-    @Override
-    public void onProviderEnabled(String provider)
-    {
-        isProviderDefined = true;
-    }
-
-    @Override
-    public void onProviderDisabled(String provider)
-    {
-        isProviderDefined = false;
     }
 }
--- frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/MainActivity.java
+++ frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/MainActivity.java
@@ -1,43 +1,76 @@
 package com.cmps115.public_defender;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
-    // Geolocation components
-    LocationManager geoManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-    GeoHandler geoHandler = new GeoHandler();
+public class MainActivity extends AppCompatActivity {
+    GeoHandler geoHandler;
 
     // To show Geo
-    TextView text;
+    TextView view_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        text = (TextView) findViewById(R.id.geotext);
-        //setupGeolocation();
         setContentView(R.layout.activity_main);
     }
 
-    private void setupGeolocation()
+    @Override
+    protected void onStart() {
+        super.onStart();
+        view_text = (TextView) findViewById(R.id.geotext);
+        setupPremissions();
+        setupGeolocation();
+    }
+
+    private void setupPremissions()
     {
-        // Setup Geolocation
-        try {
-            geoManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, geoHandler);
-        } catch(SecurityException se) {/* We don't have premission */}
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 13);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    private void setupGeolocation() {
+        geoHandler = new GeoHandler(this, (LocationManager) getSystemService(Context.LOCATION_SERVICE));
     }
 
     public void geoButtonPressed(View v)
     {
+        Log.d("public_defender", "geo button down");
         if(geoHandler.hasGeolocation())
         {
             double[] geo = geoHandler.getGeolocation();
-            text.setText(geo[0] + ", " + geo[1]);
+            view_text.setText(geo[0] + ", " + geo[1]);
+        }
+        else
+        {
+            view_text.setText("Doesn't look like you have geolocation enabled on your phone.");
         }
     }
 }
