1c984307c5b98bac7d05733eba82b3f80d184cfa
sslarkin <sslarkin@ucsc.edu>
2017-04-26 21:44:18 -0700

    Adding views, about to do a merge by hand into Develop

--- frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/MainActivity.java
+++ frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/MainActivity.java
@@ -1,11 +1,13 @@
 package com.cmps115.public_defender;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -24,18 +26,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void broadCast(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, Settings.class);
-        Log.d("Record", "recording");
-        startActivity(intent);
-
-    }
+        // Do something in response to Record button
+        Context context = getApplicationContext();
+        CharSequence text = "Hit Record";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+       }
 
     public void gotoMenu(View view){
-        Intent intent = new Intent(this, Settings.class);
-        Log.d("Settings", "clicked menu");
+        Intent intent = new Intent(this, Menu.class);
+        Log.d("Menu", "clicked menu");
         startActivity(intent);
+    }
 
+    public void gotoCurrentEvents(View view){
+        Intent intent = new Intent(this, CurrentEvents.class);
+        startActivity(intent);
     }
 
 }
