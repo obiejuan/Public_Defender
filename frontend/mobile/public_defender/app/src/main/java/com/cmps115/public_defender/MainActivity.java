4f11c0fc962f90fcc1db064b422bf851cb6ab7c1
Oliver Davies <odavies@ucsc.edu>
2017-04-28 20:06:00 -0700

    Fixed several issues, made more modular and more robust. Currently it does not work with the emulator, only the actual phone. I have a feeling it has more to do with than the emulator than myself. The design asks for premissions, takes user to location page if they don't have location enabled and over all ensures the user does everything we need to get their location.

--- frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/MainActivity.java
+++ frontend/mobile/public_defender/app/src/main/java/com/cmps115/public_defender/MainActivity.java
@@ -1,17 +1,9 @@
 package com.cmps115.public_defender;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     GeoHandler geoHandler;
@@ -29,48 +21,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         view_text = (TextView) findViewById(R.id.geotext);
-        setupPremissions();
-        setupGeolocation();
-    }
-
-    private void setupPremissions()
-    {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 13);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
-    }
-
-    private void setupGeolocation() {
-        geoHandler = new GeoHandler(this, (LocationManager) getSystemService(Context.LOCATION_SERVICE));
+        geoHandler = new GeoHandler(this);
     }
 
     public void geoButtonPressed(View v)
     {
-        Log.d("public_defender", "geo button down");
-        if(geoHandler.hasGeolocation())
-        {
-            double[] geo = geoHandler.getGeolocation();
+        double[] geo = geoHandler.get_geolocation();
+        if(!(geo[0] == 0.0 && geo[1] == 0.0))
             view_text.setText(geo[0] + ", " + geo[1]);
-        }
-        else
-        {
-            view_text.setText("Doesn't look like you have geolocation enabled on your phone.");
-        }
     }
 }
