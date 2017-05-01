5f7cf753c084706a9eabf83758ad4c2ad6bec81c
Bryan <bmccoid@ucsc.edu>
2017-05-01 12:53:05 -0700

    refactored docker config

--- backend/node/run.sh
+++ backend/node/run.sh
@@ -1,6 +1,13 @@
+cp pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf
+chown postgres /etc/postgresql/9.5/main/pg_hba.conf
+
+cp postgresql.conf /etc/postgresql/9.5/main/postgresql.conf
+chown postgres /etc/postgresql/9.5/main/postgresql.conf
+
 service postgresql start
-useradd -p "papAq5PwY/QQM" -s /bin/bash node
+
 cp database.initial /var/lib/postgresql/.
 chown postgres /var/lib/postgresql/database.initial
 su -l postgres -c "psql -f /var/lib/postgresql/database.initial"
+
 node_modules/nodemon/bin/nodemon.js app.js
